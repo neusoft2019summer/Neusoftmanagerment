4cf7520a7f31f134ac11c173f64bc4a34b3a6695
zhangziqi <xiaoqiqiyeah@126.com>
2019-08-09 11:26:36 +0800

    11:26

---
+++ src/main/java/com/neusoft/managerment/office/model/NewsModel.java
@@ -0,0 +1,21 @@
+package com.neusoft.managerment.office.model;
+
+
+
+import org.apache.ibatis.type.Alias;
+
+
+
+import lombok.Data;
+
+/**
+ *    author : 张梓琪
+ *    version: 1.0
+ *    系统基础信息模块的Model
+ */
+@Alias("NewsModel")
+@Data
+public class NewsModel {
+	
+
+}
