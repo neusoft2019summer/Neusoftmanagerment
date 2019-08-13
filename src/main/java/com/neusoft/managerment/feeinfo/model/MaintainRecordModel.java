1f1f52ffd3db5637c45ef257c5c9b4d9f5ae4f68
zhangziqi <xiaoqiqiyeah@126.com>
2019-08-14 00:57:55 +0800

    增添维护维修表

--- src/main/java/com/neusoft/managerment/feeinfo/model/MaintainRecordModel.java
+++ src/main/java/com/neusoft/managerment/feeinfo/model/MaintainRecordModel.java
@@ -5,7 +5,8 @@ import java.util.Date;
 
 import org.apache.ibatis.type.Alias;
 
-
+import com.neusoft.managerment.baseinfo.model.MaintainProviderModel;
+import com.neusoft.managerment.baseinfo.model.RoomModel;
 
 import lombok.Data;
 
@@ -18,19 +19,23 @@ import lombok.Data;
 @Data
 public class MaintainRecordModel implements Serializable {
 	private int recordno = 0;
-	private int mytypeno = 0;
-	private int roomno = 0;
+	//外键
+	private M_typeModel mtypeno = null;
+	//外键
+	private RoomModel roomno = null;
 	private String contactname = null;
 	private String mobile = null;
 	private String tel = null;
 	private Date mdate = null;
 	private String mdesc = null;
-	private String wempid = null;
+	//外键
+	private WempidModel wempid = null;
 	private Date wdate = null;
 	private String wtask = null;
 	private String wresult = null;
 	private int wfee = 0;
-	private int prociderno = 0;
+	//外键
+	private MaintainProviderModel prociderno = null;
 	private String clientfeeback = null;
 
 }
