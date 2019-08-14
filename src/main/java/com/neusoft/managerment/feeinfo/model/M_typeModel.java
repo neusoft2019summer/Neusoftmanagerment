538f23a7e7ee8f5a0d2c26cbed5fe525ac99e9c6
zhangziqi <xiaoqiqiyeah@126.com>
2019-08-14 13:39:17 +0800

    增加了维修类型

--- src/main/java/com/neusoft/managerment/feeinfo/model/M_typeModel.java
+++ src/main/java/com/neusoft/managerment/feeinfo/model/M_typeModel.java
@@ -1,10 +1,25 @@
 package com.neusoft.managerment.feeinfo.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
+
+import com.neusoft.managerment.baseinfo.model.MaintainProviderModel;
+import com.neusoft.managerment.baseinfo.model.RoomModel;
+
+import lombok.Data;
+/**
+ *    author : 张梓琪
+ *    version: 1.0
+ *    物业日常业务管理模块的维修Model 
+ */
+@Alias("mtype")
+@Data
 public class M_typeModel implements Serializable{
 
 	//维修类型
-	private int MTypeNo = 0;
+	private int mtypeno = 0;
+	private String mtypename = null;
 	
 }
