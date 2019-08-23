b4de36f3f792d4b47f23690251acc611aa0b364b
chensiying <954310051@qq.com>
2019-08-23 15:25:51 +0800

    房间完成

--- src/main/java/com/neusoft/managerment/baseinfo/model/HouseTypeModel.java
+++ src/main/java/com/neusoft/managerment/baseinfo/model/HouseTypeModel.java
@@ -1,10 +1,14 @@
 package com.neusoft.managerment.baseinfo.model;
 
 import java.io.Serializable;
-
 import org.apache.ibatis.type.Alias;
-
 import lombok.Data;
+
+/**
+ *    模块：基础信息   表：户型HouseType
+ *    author : 陈思颖
+ *    version: 1.0
+ */
 
 @Alias("housetype")
 @Data
